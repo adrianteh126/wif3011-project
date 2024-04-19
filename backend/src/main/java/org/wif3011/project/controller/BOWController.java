45690d0ade5d38eb0410dc85f555342ee2087df8
adrianteh126 <91460516+adrianteh126@users.noreply.github.com>
2024-04-19 17:32:46 +0800

    Add sorting and limit params for api (#7)
    * remove unused code
    
    * move timer to service
    
    * add sorting and limit helper
    
    * sequential: add sorting and limit params

--- backend/src/main/java/org/wif3011/project/controller/BOWController.java
+++ backend/src/main/java/org/wif3011/project/controller/BOWController.java
@@ -2,10 +2,7 @@ package org.wif3011.project.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.wif3011.project.service.BagOfWordService;
 import org.wif3011.project.service.SecondConcurrentService;
@@ -25,19 +22,16 @@ public class BOWController {
     private final SecondConcurrentService secondConcurrentService;
 
     @PostMapping(ApiConstant.SEQUENTIAL_BOW)
-    public ResponseEntity<Object> getSequentialWordMap(@RequestBody MultipartFile file) {
+    public ResponseEntity<Object> getSequentialWordMap(
+            @RequestParam("numOfWords") int numOfWords,
+            @RequestParam("sortAscending") boolean sortAscending,
+            @RequestBody MultipartFile file) {
+
         if (file.isEmpty()) return ResponseEntity.badRequest().body("{\"error\": \"File is empty.\"}");
         if (!file.getOriginalFilename().toLowerCase().endsWith(".txt"))
             return ResponseEntity.badRequest().body("{\"error\": \"Invalid file format.\"}");
 
-        Timer timer = new Timer();
-        timer.start();
-        Map<String, Integer> wordMap = sequentialBOWService.sequentialWordMap(file);
-        timer.stop();
-
-        Map<String, Object> body = new HashMap<>();
-        body.put("elapsed_time", timer.getElapsedTimeMillis());
-        body.put("data", wordMap);
+        Map<String, Object> body = sequentialBOWService.sequentialWordMap(file, numOfWords, sortAscending);
         return ResponseEntity.ok(body);
     }
 
--- backend/src/main/java/org/wif3011/project/service/SequentialBOWServiceImpl.java
+++ backend/src/main/java/org/wif3011/project/service/SequentialBOWServiceImpl.java
@@ -4,7 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.wif3011.project.utility.ProcessFileServiceUtil;
+import org.wif3011.project.utility.ResponseManipulator;
+import org.wif3011.project.utility.Timer;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
@@ -14,8 +17,24 @@ public class SequentialBOWServiceImpl implements SequentialBOWService {
     private final ProcessFileServiceUtil processFileUtil;
 
     @Override
-    public Map<String, Integer> sequentialWordMap(MultipartFile file) {
+    public Map<String, Object> sequentialWordMap(MultipartFile file, int numOfWords, boolean sortAscending) {
+        long totalElapsedTime = 0;
+        Timer timer = new Timer();
+
+        timer.start();
         String document = processFileUtil.convertFileToString(file);
-        return processFileUtil.sequentialGenerateWordMap(document);
+        timer.stop();
+        totalElapsedTime += timer.getElapsedTimeMillis();
+
+
+        timer.start();
+        Map<String, Integer> data = processFileUtil.sequentialGenerateWordMap(document);
+        timer.stop();
+        totalElapsedTime += timer.getElapsedTimeMillis();
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("elapsed_time", totalElapsedTime);
+        body.put("data", ResponseManipulator.sortAndLimit(data, numOfWords, sortAscending));
+        return body;
     }
 }
