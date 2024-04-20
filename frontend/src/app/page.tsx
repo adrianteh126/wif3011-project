9eee2b2df8ecb882ee6887bae2a74ca8d3ff19b7
adrianteh126 <91460516+adrianteh126@users.noreply.github.com>
2024-04-21 00:13:14 +0800

    Add toggle and slider for query params (#8)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,15 @@
 'use client'
 
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { useToast } from '@/components/ui/use-toast'
 import { LineMdLoadingTwotoneLoop } from '@/components/icon/LineMdLoadingTwotoneLoop'
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { Switch } from '@/components/ui/switch'
+import { Slider } from '@/components/ui/slider'
 
 interface Result {
   data: {
@@ -23,19 +18,21 @@ interface Result {
   elapsed_time: number
 }
 
-type Algorithm =
-  | 'sequential'
-  | 'concurrent-java-stream'
-  | 'concurrent-fork-join'
+type Algorithm = 'sequential' | 'concurrent-java-stream' | 'concurrent-fork-join'
 
 export default function Home() {
   const baseUrl = 'http://localhost:4000'
   const { toast } = useToast()
 
-  const [selectedFile, setSelectedFile] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
-  const [data, setData] = useState<Result>()
-  const [selectOption, setSelectedOption] = useState<Algorithm>('sequential')
+  // ui
+  const [selectedFile, setSelectedFile] = useState(null) // user select file
+  const [isLoading, setIsLoading] = useState(false) // handleSubmit loading indicator
+  // fetch api
+  const [data, setData] = useState<Result>() // data fetched from request
+  const [selectOption, setSelectedOption] = useState<Algorithm>('sequential') // fetch api select options
+  // query params: result query
+  const [isAscending, setIsAscending] = useState(false) // sorting, default: descending
+  const [numOfWords, setNumOfWords] = useState(50) // return numOfWords, default: 50
 
   const handleFileChange = (event: any) => {
     setSelectedFile(event.target.files[0])
@@ -50,6 +47,7 @@ export default function Home() {
       return setIsLoading(false)
     }
 
+    // request body: upload file
     const formData = new FormData()
     formData.append('file', selectedFile)
 
@@ -58,13 +56,13 @@ export default function Home() {
 
       switch (selectOption) {
         case 'sequential':
-          fetchUrl = `${baseUrl}/api/sequential`
+          fetchUrl = `${baseUrl}/api/sequential?numOfWords=${numOfWords}&sortAscending=${isAscending}`
           break
         case 'concurrent-java-stream':
-          fetchUrl = `${baseUrl}/api/concurrent/java-stream`
+          fetchUrl = `${baseUrl}/api/concurrent/java-stream?numOfWords=${numOfWords}&sortAscending=${isAscending}`
           break
         case 'concurrent-fork-join':
-          fetchUrl = `${baseUrl}/api/concurrent/fork-join`
+          fetchUrl = `${baseUrl}/api/concurrent/fork-join?numOfWords=${numOfWords}&sortAscending=${isAscending}`
           break
       }
 
@@ -95,6 +93,14 @@ export default function Home() {
     setSelectedOption(value)
   }
 
+  const handleSortChange = (value: boolean) => {
+    setIsAscending(value)
+  }
+
+  const handleNumOfWordsChange = (value: number[]) => {
+    setNumOfWords(value[0])
+  }
+
   return (
     <main className="container flex flex-col justify-center items-center min-h-dvh">
       <div className="w-2/3 py-8">
@@ -103,9 +109,7 @@ export default function Home() {
         <Card>
           <CardHeader>
             <CardTitle>Upload File</CardTitle>
-            <CardDescription>
-              Upload your .txt file here. Not exceeding 10mb.
-            </CardDescription>
+            <CardDescription>Upload your .txt file here. Not exceeding 10mb.</CardDescription>
           </CardHeader>
           <form onSubmit={handleSubmit}>
             <CardContent className="flex flex-col gap-6">
@@ -113,33 +117,35 @@ export default function Home() {
                 <Label htmlFor="text-file">Text File</Label>
                 <Input id="text-file" type="file" onChange={handleFileChange} />
               </div>
-              <RadioGroup
-                defaultValue="sequential"
-                onValueChange={handleOptionChange}
-              >
+              <RadioGroup defaultValue="sequential" onValueChange={handleOptionChange}>
                 <div className="flex items-center space-x-2">
                   <RadioGroupItem value="sequential" id="option-sequential" />
                   <Label htmlFor="option-sequential">Sequential</Label>
                 </div>
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem
-                    value="concurrent-java-stream"
-                    id="option-concurrent"
-                  />
-                  <Label htmlFor="option-concurrent">
-                    Concurrent / JavaStream
-                  </Label>
+                  <RadioGroupItem value="concurrent-java-stream" id="option-concurrent" />
+                  <Label htmlFor="option-concurrent">Concurrent / JavaStream</Label>
                 </div>
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem
-                    value="concurrent-fork-join"
-                    id="option-concurrent-2"
-                  />
-                  <Label htmlFor="option-concurrent-2">
-                    Concurrent / ForkJoin
-                  </Label>
+                  <RadioGroupItem value="concurrent-fork-join" id="option-concurrent-2" />
+                  <Label htmlFor="option-concurrent-2">Concurrent / ForkJoin</Label>
                 </div>
               </RadioGroup>
+              <div className="flex items-center space-x-2">
+                <Switch id="sort-switch" onCheckedChange={handleSortChange} />
+                <Label htmlFor="sort-switch">Sort Ascending</Label>
+              </div>
+              <div className="flex flex-col gap-3">
+                <Label htmlFor="slider">Number of Words: {numOfWords}</Label>
+                <Slider
+                  id="slider"
+                  className="w-[30%]"
+                  defaultValue={[50]}
+                  max={100}
+                  step={1}
+                  onValueChange={handleNumOfWordsChange}
+                />
+              </div>
             </CardContent>
             <CardFooter>
               <Button type="submit" disabled={isLoading}>
